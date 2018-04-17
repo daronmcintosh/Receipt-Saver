7bb4a00071e8331af0b9fd9649d5381f107e380d
Daron McIntosh <mcintoshdaron@gmail.com>
2018-04-17 12:14:32 -0400

    update database helper

--- app/src/main/java/com/example/daron/receiptsaver/DatabaseHelper.java
+++ app/src/main/java/com/example/daron/receiptsaver/DatabaseHelper.java
@@ -5,9 +5,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    
-    private static final String DB_NAME = "receiptinfo"; // the name of our database
-    private static final int DB_VERSION = 1; // the version of the database
+
+    public static final String DB_NAME = "ReceiptInfo"; // the name of our database
+    public static final int DB_VERSION = 1; // the version of the database
 
     DatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
@@ -18,8 +18,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     }
 
+
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if (oldVersion < 1) {
+            db.execSQL("CREATE TABLE RECEIPTINFO (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "CATEGORY TEXT, "
+                    + "DATE TEXT, "
+                    + "TOTAL REAL, "
+                    + "DESCRIPTION TEXT);");
+//            insertSong(db, StreamMusicFragment.defaultURL, true);
+        }
     }
 }
